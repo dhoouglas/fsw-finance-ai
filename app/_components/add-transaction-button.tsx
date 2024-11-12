4a795224f620fe4c3456c2a1958bc90e5dedaa74
dhoouglas <dhoouglas@hotmail.com>
2024-11-12 17:31:16 -0300

    feat: ensure user can not create transaction on free plan if above limit

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -4,19 +4,43 @@ import { useState } from "react";
 import UpsertTransactionDialog from "./upsert-transaction-dialog";
 import { Button } from "./ui/button";
 import { ArrowDownUpIcon } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
-const AddTransactionButton = () => {
+interface AddTransactionButtonProps {
+  userCanAddTransaction?: boolean;
+}
+
+const AddTransactionButton = ({
+  userCanAddTransaction,
+}: AddTransactionButtonProps) => {
   const [dialogIsOpen, setDialogIsOpen] = useState(false);
 
   return (
     <>
-      <Button
-        className="rounded-full font-bold"
-        onClick={() => setDialogIsOpen(true)}
-      >
-        Adicionar trasações
-        <ArrowDownUpIcon />
-      </Button>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              className="rounded-full font-bold"
+              onClick={() => setDialogIsOpen(true)}
+              disabled={!userCanAddTransaction}
+            >
+              Adicionar trasações
+              <ArrowDownUpIcon />
+            </Button>
+          </TooltipTrigger>
+
+          <TooltipContent>
+            {!userCanAddTransaction &&
+              "Você atingiu o limite de transações. Atualize seu plano para criar transações ilimitadas."}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
 
       <UpsertTransactionDialog
         isOpen={dialogIsOpen}
